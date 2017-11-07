29803c85a2c25702fe239a35677e9343f2782c88
DesC <cahill.desmond@gmail.com>
2017-11-07 14:16:34 +0000

    chart styling

--- static/js/graph.js
+++ static/js/graph.js
@@ -9,7 +9,16 @@
         d.Global = parseFloat(d.Global);
     });
     
-
+// Pie Chart
+    var manupie_dim = ndx.dimension(dc.pluck("Manufacturer"));
+    var totsales = manupie_dim.group().reduceSum(dc.pluck("Global"));
+    
+    dc.pieChart("#piechart")
+        .height(300)
+        .radius(150)
+        .transitionDuration(1500)
+        .dimension(manupie_dim)
+        .group(totsales);
 
 // Manufacturer Sales
     var manufacturerdim = ndx.dimension(dc.pluck("Manufacturer"));
@@ -28,22 +37,6 @@
         .xAxisLabel("Manufacturer")
         .yAxis().ticks(2);
 
-// Genre Sales
-    var genredim = ndx.dimension(dc.pluck("Genre"));
-    var totalsales = genredim.group().reduceSum(dc.pluck("Global"));
-
-    dc.barChart("#genre-sales")
-        .height(500)
-        .width(500)
-        .margins({top: 10, right: 10, bottom: 30, left: 10})
-        .dimension(genredim)
-        .group(totalsales)
-        .transitionDuration(500)
-        .x(d3.scale.ordinal())
-        .xUnits(dc.units.ordinal)
-        .xAxisLabel("Genre")
-        .yAxis().ticks(2);
-
 // Year Sales
     var yeardim = ndx.dimension(dc.pluck("Year"));
     var totalsales = yeardim.group().reduceSum(dc.pluck("Global"));
@@ -60,32 +53,44 @@
         .xAxisLabel("Year")
         .yAxis().ticks(2);
 
-// Pie Chart
-    var manupie_dim = ndx.dimension(dc.pluck("Manufacturer"));
-    var totsales = manupie_dim.group().reduceSum(dc.pluck("Global"));
-    
-    dc.pieChart("#piechart")
-        .height(330)
-        .radius(270)
-        .transitionDuration(1500)
-        .dimension(manupie_dim)
-        .group(totsales);
+// Genre Sales
+    var genredim = ndx.dimension(dc.pluck("Genre"));
+    var totalsales = genredim.group().reduceSum(dc.pluck("Global"));
+
+    dc.barChart("#genre-sales")
+        .height(500)
+        .width(800)
+        .margins({top: 10, right: 10, bottom: 30, left: 10})
+        .dimension(genredim)
+        .group(totalsales)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Genre")
+        .yAxis().ticks(2);
         
-// Row Chart
+// Publisher Sales - Row Chart
     var publisherdim = ndx.dimension(dc.pluck("Publisher"));
     var totalsales = publisherdim.group().reduceSum(dc.pluck("Global"));
     var chart = dc.rowChart("#pub-sales");
         chart
-            .width(1200)
-            .height(8000)
+            .width(900)
+            .height(2000)
             .dimension(publisherdim)
             .group(totalsales)
+            .cap(100)
+            .othersGrouper(false)
             .xAxis().ticks(7);
+            
 
 
  
     dc.renderAll();
 }    
+
+// set all text to white
+// d3.selectAll("#gender_divide text").style("fill", "white");
+
 
 // // Publisher Sales
 //     var publisherdim = ndx.dimension(dc.pluck("Publisher"));
